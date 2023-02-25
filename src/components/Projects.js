9d6fbd3ab440ae7b703c77fb44fad12990e6f91c
salwa rzki <salwarzki@gmail.com>
2023-02-25 18:21:03 +0700

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,46 @@
+import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            Apps I've Built and Working In Company
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            I'm a software engineer with 3-year experience. As a skilled Software Engineer employee, I would like to offer myself as a candidate for the job. It’s been almost 3 years since I’ve been working as Software Engineer, Front End Developer, and Full Stack Web Developer. In these 3 years, I have worked with many people and companies, making me a skilled and experienced Software Engineer worker. My typing speed has also proven satisfactory to all my old clients.
+          </p>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="sm:w-1/2 w-100 p-4">
+              <div className="flex relative">
+                <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src={project.image}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    title: "Build Web Education",
+    subtitle: "Freelance",
+    description:
+      " Designed and developed a personal website with attention to Core Web Vitals and achieved 100 scores for Performance",
+    image: "./project-1.gif",
+    link: "https://alwaysngoding.com/",
+  },
+  {
+    title: "LiquidPedia",
+    subtitle: "Freelance",
+    description:
+      "Helped the team achieve a tight migration schedule by developing 4 internal tools in 2 months with attention to unit teste, user acces control, and code quality.",
+    image: "./project-2.gif",
+    link: "https://liquipedia.net/",
+  },
+  {
+    title: "Detik News",
+    subtitle: "Working in Detik news",
+    description:
+      "Developed a blog feture (Static Site Rendering) to improve web SEO and perfomance (React).",
+    image: "project-3.gif",
+    link: "https://news.detik.com/",
+
+  },
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+  "Tailwind",
+  "Bootstrap"
+];
